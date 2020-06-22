12a5b28b4032aaaefca59752f44fe0912d27c374
renanfelice <felice_owns@hotmail.com>
2020-06-22 00:04:08 -0300

    responsividade navbar

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -5,13 +5,15 @@ const Flashcard = () => {
 
     const [isFlipping, setIsFlipping] = useState(false)
     const [flip, setFlip] = useState(false)
-    return (<div onClick={() => {
+    return (
+    
+    <div onClick={() => {
         if(isFlipping) return
         setIsFlipping(true)
         setFlip(!flip)
-        // setTimeout(() => {
-        //     setIsFlipping(false)
-        // }, 300);
+        setTimeout(() => {
+            setIsFlipping(false)
+        }, 300);
         
     }} className={`Flashcard ${flip ? 'flip' : ''}`}>
         <div className='front'>
@@ -20,7 +22,8 @@ const Flashcard = () => {
             <div className='option'>opção do front 2</div>
         </div>
         <div className='back'>esse é o back, resposta aqui</div>
-    </div>);
+    </div>
+    );
 }
 
 export default Flashcard;
